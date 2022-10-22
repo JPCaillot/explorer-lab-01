8224cf11f1b4162100f18cd0734c454d90ab0262
Joao Pedro Caillot <joaopcaillot@gmail.com>
2022-10-22 11:31:21 -0300

    classifying cards according to regex

--- src/main.js
+++ src/main.js
@@ -44,3 +44,43 @@ const expirationDatePattern = {
   }
 }
 const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+
+//card number masks
+//visa - starts with 4 with 15 moore digits
+// ^4\d{0,15}
+//mastercard - starts with 5 followed by a 1 to 5 digit, followed by 2 more digits
+//OR starts with 22 followed by a 2 to 9 digit, followed by one more digit
+//OR starts with 2 followed by a 3 to 7 digit, followed by 2 more digits
+//followed by 12 more digits
+// (^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}
+
+const cardNumber = document.querySelector("#card-number")
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15/,
+      cardtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+      cardtype: "mastercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: "default",
+    },
+  ],
+  dispatch: function(appended, dynamicMasked){ //everytime we click the number input
+    const number = (dynamicMasked.value + appended).replace(/\D/g, "")
+    const foundMask = dynamicMasked.compiledMasks.find(function(item) {
+
+      return number.match(item.regex)
+    })
+    console.log(foundMask) //showing partial results on devtools
+
+    return foundMask
+  },
+}
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
